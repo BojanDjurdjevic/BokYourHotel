54eda1d12ef10432e7f360fed28a26673e777237
BojanDjurdjevic <pininfarina164@gmail.com>
2026-07-08 07:01:41 +0200

    BookingService created & empty methods design made

---
+++ app/Services/BookingService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Room;
+
+class BookingService {
+
+    public function createBooking(Room $room, array $data) 
+    {
+
+    }
+
+    public function validateDates()
+    {
+
+    }
+
+    public function loadInventories()
+    {
+
+    }
+
+    public function ensureAvailability()
+    {
+
+    }
+
+    public function calculatePrice()
+    {
+
+    }
+
+    public function createBookingRecord()
+    {
+
+    }
+
+    public function decreaseAvailability()
+    {
+
+    }
+    
+
+}
